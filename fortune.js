6ffae1c5f00e99004ca6b91490d544c15d51c244
Kaylee Frank <frankaylee10@gmail.com>
2026-05-05 00:50:19 -0500

    :sparkles: added custom module for fortunes

--- fortune.js
+++ fortune.js
@@ -4,5 +4,4 @@ const fortunes = require('./fortune.json')
 module.exports = function getFortune() {
     const randomIndex = Math.floor(Math.random() * fortunes.length)
     return fortunes[randomIndex]
-    
 }
